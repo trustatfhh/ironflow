eaab1e48b508449bbd5d188ab568163a8c854594
Bastian Hellmann <bahellma@googlemail.com>
2014-09-10 12:22:55 +0200

    [release] version 0.0.3

--- src/main/java/de/hshannover/f4/trust/ironflow/publisher/strategies/RequestDeviceList.java
+++ src/main/java/de/hshannover/f4/trust/ironflow/publisher/strategies/RequestDeviceList.java
@@ -18,7 +18,7 @@
  * Email: trust@f4-i.fh-hannover.de
  * Website: http://trust.f4.hs-hannover.de
  * 
- * This file is part of ironflow, version 0.0.1, implemented by the Trust@HsH
+ * This file is part of ironflow, version 0.0.3, implemented by the Trust@HsH
  * research group at the Hochschule Hannover.
  * %%
  * Copyright (C) 2013 - 2014 Trust@HsH
