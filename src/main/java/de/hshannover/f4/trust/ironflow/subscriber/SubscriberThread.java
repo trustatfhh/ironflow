052edcc53a5598773418774f63be164f16f413ab
Bastian Hellmann <bahellma@googlemail.com>
2015-03-04 12:38:53 +0100

    [release] version 0.0.5

--- src/main/java/de/hshannover/f4/trust/ironflow/subscriber/SubscriberThread.java
+++ src/main/java/de/hshannover/f4/trust/ironflow/subscriber/SubscriberThread.java
@@ -18,7 +18,7 @@
  * Email: trust@f4-i.fh-hannover.de
  * Website: http://trust.f4.hs-hannover.de
  * 
- * This file is part of ironflow, version 0.0.4, implemented by the Trust@HsH
+ * This file is part of ironflow, version 0.0.5, implemented by the Trust@HsH
  * research group at the Hochschule Hannover.
  * %%
  * Copyright (C) 2013 - 2015 Trust@HsH
